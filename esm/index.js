aa94fc75ada8bbbbe007a6dedcf706285cab6b0f
Liviu Ionescu <ilg@livius.net>
2023-03-06 20:48:20 +0200

    add the esm folder

---
+++ esm/index.js
@@ -0,0 +1,17 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2017 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/licenses/MIT/.
+ */
+
+// ----------------------------------------------------------------------------
+
+// Re-export the library definitions as from a module.
+export * from '../cjs/index.js'
+
+// ----------------------------------------------------------------------------
