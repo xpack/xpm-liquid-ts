da3e8c18a497e6dc18c7ba04221827928d8f14f0
Liviu Ionescu <ilg@livius.net>
2025-12-27 00:33:11 +0200

    add errors.ts

---
+++ src/lib/errors.ts
@@ -0,0 +1,25 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2021-2025 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/license/mit.
+ */
+
+/* eslint max-len: [ "error", 80, { "ignoreUrls": true } ] */
+
+// ----------------------------------------------------------------------------
+
+// Application errors
+export class XpmError extends Error {}
+
+export class XpmPrerequisitesError extends Error {}
+
+export class XpmInputError extends Error {}
+
+// Other errors: Syntax, Output, Child.
+
+// ----------------------------------------------------------------------------
