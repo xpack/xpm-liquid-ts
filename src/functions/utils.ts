7efd06436da6a5b6719c5d6258d8203a31eda58d
Liviu Ionescu <ilg@livius.net>
2026-01-27 20:28:56 +0200

    getPlatformKey more generic

--- src/functions/utils.ts
+++ src/functions/utils.ts
@@ -98,11 +98,9 @@ export function getPlatformKey({
   const platform = process.platform
   let arch = process.arch
   if (doForce32bit) {
-    if (platform === 'win32' && arch === 'x64') {
+    if (arch === 'x64') {
       arch = 'ia32'
-    } else if (platform === 'linux' && arch === 'x64') {
-      arch = 'ia32'
-    } else if (platform === 'linux' && arch === 'arm64') {
+    } else if (arch === 'arm64') {
       arch = 'arm'
     }
   }
