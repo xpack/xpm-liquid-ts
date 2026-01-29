4413130199035b3aca15ff01aaa0ccd63bfe6fee
Liviu Ionescu <ilg@livius.net>
2026-01-29 15:43:36 +0200

    policies.ts cosmetics

--- src/classes/policies.ts
+++ src/classes/policies.ts
@@ -153,20 +153,21 @@ export class XpmPolicies {
   constructor({ log, minVersion }: { log: Logger; minVersion: string }) {
     log.trace(`${XpmPolicies.name}({minVersion: ${minVersion})`)
 
-    if (semver.valid(minVersion) === null) {
-      return
+    if (semver.valid(minVersion) !== null) {
+      this.minVersion = minVersion
+
+      this.shareNpmDependencies = semver.lt(this.minVersion, '0.14.0')
+
+      this.nonHierarchicalLocalXpacksFolder = semver.lt(
+        this.minVersion,
+        '0.16.0'
+      )
+      this.onlyStringDependencies = semver.lt(this.minVersion, '0.16.0')
+
+      this.singleParameterXpmInitTemplate = semver.lt(this.minVersion, '0.22.0')
     }
 
-    this.minVersion = minVersion
-
-    this.shareNpmDependencies = semver.lt(this.minVersion, '0.14.0')
-    this.nonHierarchicalLocalXpacksFolder = semver.lt(this.minVersion, '0.16.0')
-    this.onlyStringDependencies = semver.lt(this.minVersion, '0.16.0')
-    this.singleParameterXpmInitTemplate = semver.lt(this.minVersion, '0.22.0')
-
-    log.trace(
-      `policies.shareNpmDependencies: ${String(this.shareNpmDependencies)}`
-    )
+    log.trace('policies:', this)
   }
 }
 
