f5713f51438dc5b01d8a1d44b13fa5e5a52b2068
Liviu Ionescu <ilg@livius.net>
2026-02-03 01:01:58 +0200

    tests renumber

---
+++ tests/tap/classes/640-init-template-base.ts
@@ -0,0 +1,39 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2021-2026 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/license/mit.
+ */
+
+/* eslint max-len: [ "error", 80, { "ignoreUrls": true } ] */
+
+// ----------------------------------------------------------------------------
+
+// import * as os from 'node:os'
+
+// ----------------------------------------------------------------------------
+
+// https://www.npmjs.com/package/tap
+import { test } from 'tap'
+
+import { Logger } from '@xpack/logger'
+
+// ----------------------------------------------------------------------------
+
+import { XpmPolicies } from '../../../src/index.js'
+
+// ----------------------------------------------------------------------------
+
+const log = new Logger({ level: 'info' })
+
+// ----------------------------------------------------------------------------
+
+await test('XpmInitTemplateBase', async (t): Promise<void> => {
+  t.end()
+})
+
+// ----------------------------------------------------------------------------
