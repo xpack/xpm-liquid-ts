82ce65b280fb4c068bcc036e75c66c8a48f6e621
Liviu Ionescu <ilg@livius.net>
2026-02-05 23:21:47 +0200

    tests: add coverage-map

---
+++ tests/coverage-map.js
@@ -0,0 +1,27 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2021-2026 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/license/mit.
+ */
+
+/* eslint max-len: [ "error", 80, { "ignoreUrls": true } ] */
+
+// ----------------------------------------------------------------------------
+
+// https://tapjs.org/coverage/#coverage-maps
+
+// # If it returns a string or string array, then it will only generate 
+// coverage for the file(s) listed.
+
+export default function getCoverageMapFiles (testFilePath) { 
+  const sourceFilePath = testFilePath.replace(/tests\/tap/, 'src')
+  // console.log(`Getting coverage map for source file: ${sourceFilePath}`)
+  return sourceFilePath
+}
+
+// ----------------------------------------------------------------------------
