baebab9ae298e077971962ce1e421b731d7b1dee
Liviu Ionescu <ilg@livius.net>
2026-02-21 22:08:52 +0200

    re-generate commons

--- config/eslint.config.js
+++ config/eslint.config.js
@@ -1,3 +1,19 @@
+/*
+ * DO NOT EDIT!
+ * Automatically generated from npm-packages-helper/templates/*.
+ * 
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2021-2026 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/license/mit.
+ */
+
+// ----------------------------------------------------------------------------
+
 // https://typescript-eslint.io/packages/typescript-eslint/
 // https://eslint.org/docs/latest/use/configure/configuration-files
 
